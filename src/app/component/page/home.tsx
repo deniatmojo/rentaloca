c9663c39f6da719c20a05665193d6b8703fd35c6
deniatmojo <deni.atmojo@ymail.com>
2024-11-21 03:56:43 +0700

    Merapikan folder

---
+++ src/app/component/page/home.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import HeaderHome from "../layouts/header";
+import Howitwork from "../layouts/howitwork";
+import Wardobe from "../layouts/wardobe";
+import Lorem from "../layouts/lorem";
+import Footer from "../layouts/footer";
+import Faq from "../layouts/faq";
+import Featured from "../layouts/featured";
+import Testimoni from "../layouts/testimoni";
+import Featuredon from "../layouts/featuredon";
+import Demand from "../layouts/demand";
+
+const home = () => {
+  return (
+    <div>
+      <HeaderHome />
+      <Howitwork />
+      <Wardobe />
+      <Lorem />
+      <Featured />
+      <Testimoni />
+      <Featuredon />
+      <Demand />
+      <Faq />
+      <Footer />
+    </div>
+  );
+};
+
+export default home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import HeaderHome from "./component/layouts/header";
-import Howitwork from "./component/layouts/howitwork";
-import Wardobe from "./component/layouts/wardobe";
-import Lorem from "./component/layouts/lorem";
-import Footer from "./component/layouts/footer";
-import Faq from "./component/layouts/faq";
-import Featured from "./component/layouts/featured";
-import Testimoni from "./component/layouts/testimoni";
-import Featuredon from "./component/layouts/featuredon";
-import Demand from "./component/layouts/demand";
+import HeaderHome from "./component/page/home";
 
 export default function header() {
   return (
     <div>
       <HeaderHome />
-      <Howitwork />
-      <Wardobe />
-      <Lorem />
-      <Featured />
-      <Testimoni />
-      <Featuredon />
-      <Demand />
-      <Faq />
-      <Footer />
     </div>
   );
 }
