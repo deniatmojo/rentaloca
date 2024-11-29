db7213ca83a63b7893d9fc1ca73bf32546c2a4ba
deniatmojo <deni.atmojo@ymail.com>
2024-11-29 10:53:41 +0700

    Slicing catalog selesai

---
+++ src/app/component/elements/ClothingCard.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+interface ClothingCardProps {
+  name: string;
+  image: string;
+  price: string;
+}
+
+const ClothingCard = ({ name, image, price }: ClothingCardProps) => {
+  return (
+    <div className="flex flex-col h-[481px]">
+      <img
+        className="w-[333px] h-[405px] rounded-md object-cover"
+        src={image}
+        alt={name}
+      />
+      <h2 className="text-black font-beautiqueMed mt-1 text-lg">{name}</h2>
+      <h3 className="text-black font-Inter mt-1 mb-2 text-sm">Rp. {price}</h3>
+    </div>
+  );
+};
+
+export default ClothingCard;
